5d218b4a74421b2c8a00488b0efd97ba3f8caade
Lawrence-Tang <tangzongsheng@gmail.com>
2022-12-03 00:26:26 +0800

    use make_ext4fs to gen userdata.img

--- tools/update_prebuilt.sh
+++ tools/update_prebuilt.sh
@@ -11,23 +11,12 @@ cp -f $2/idbloader.img $1/
 cp -f $2/misc.img $1/
 cp -f $2/dtbo.img $1/
 
-TOP=$PWD
-export MKE2FS_CONFIG="${TOP}/tools/mke2fs.conf"
-if [ ! -f ${MKE2FS_CONFIG} ]; then
-    echo "error: ${MKE2FS_CONFIG} not found."
-    exit 1
-fi
-true ${MKFS:="${TOP}/tools/mke2fs"}
-
 RET=0
 if [ ! -f $1/userdata.img ]; then
 	USERDATA_SIZE=104857600
 	echo "Generating empty userdata.img (size:${USERDATA_SIZE})"
 	TMPDIR=`mktemp -d`
-    IMG_BLK=$((${USERDATA_SIZE} / 4096))
-    [ -f $1/userdata.img ] && rm -f $1/userdata.img
-	INODE_SIZE=256
-    ${MKFS} -N ${INODE_SIZE} -E android_sparse -t ext4 -L userdata -M /root -b 4096 -d ${TMPDIR} $1/userdata.img ${IMG_BLK}
+	${PWD}/tools/make_ext4fs -s -l ${USERDATA_SIZE} -a root -L userdata $1/userdata.img ${TMPDIR}
 	RET=$?
 	rm -rf ${TMPDIR}
 fi
