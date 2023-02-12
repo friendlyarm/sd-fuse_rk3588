21dd9dff3f8b7bdf6a9797a778a9ffd342c17190
Lawrence-Tang <tangzongsheng@gmail.com>
2023-02-12 17:14:10 +0800

    fix toolchain

--- build-kernel.sh
+++ build-kernel.sh
@@ -157,9 +157,9 @@ fi
 
 if [ ! -d /opt/FriendlyARM/toolchain/11.3-aarch64 ]; then
 	echo "please install aarch64-gcc-11.3 first, using these commands: "
-	echo "\tgit clone https://github.com/friendlyarm/prebuilts.git -b master --depth 1"
-	echo "\tcd prebuilts/gcc-x64"
-	echo "\tcat toolchain-6.4-aarch64.tar.gz* | sudo tar xz -C /"
+	echo "    git clone https://github.com/friendlyarm/prebuilts.git -b master --depth 1"
+	echo "    cd prebuilts/gcc-x64"
+	echo "    sudo tar xvf toolchain-11.3-aarch64.tar.xz -C /"
 	exit 1
 fi
 
