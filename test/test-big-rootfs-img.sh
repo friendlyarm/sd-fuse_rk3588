cc9361d537d6c2f0390e11cd69fe28e8a31e91d1
Lawrence-Tang <tangzongsheng@gmail.com>
2024-07-05 11:12:08 +0800

    update the example script to show how to make image with a large rootfs

--- test/test-big-rootfs-img.sh
+++ test/test-big-rootfs-img.sh
@@ -13,19 +13,20 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_rk3588
 cd sd-fuse_rk3588
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/images-for-eflasher/debian-bullseye-desktop-arm64-images.tgz
-tar xzf debian-bullseye-desktop-arm64-images.tgz
+wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/images-for-eflasher/ubuntu-focal-desktop-arm64-images.tgz
+tar xzf ubuntu-focal-desktop-arm64-images.tgz
 wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/images-for-eflasher/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
 
-# make big file
-fallocate -l 5G debian-bullseye-desktop-arm64/rootfs.img
+# 20G rootfs
+fallocate -l 20G ubuntu-focal-desktop-arm64/rootfs.img
 
 # calc image size
-IMG_SIZE=`du -s -B 1 debian-bullseye-desktop-arm64/rootfs.img | cut -f1`
+IMG_SIZE=`du -s -B 1 ubuntu-focal-desktop-arm64/rootfs.img | cut -f1`
 
 # re-gen parameter.txt
-./tools/generate-partmap-txt.sh ${IMG_SIZE} debian-bullseye-desktop-arm64
+./tools/generate-partmap-txt.sh ${IMG_SIZE} ubuntu-focal-desktop-arm64
 
-./mk-sd-image.sh debian-bullseye-desktop-arm64
-sudo ./mk-emmc-image.sh debian-bullseye-desktop-arm64
+# The image can only be written to a TF card that is 32GB or bigger
+RAW_SIZE_MB=30000 ./mk-sd-image.sh ubuntu-focal-desktop-arm64
+sudo bash -c 'RAW_SIZE_MB=30000 ./mk-emmc-image.sh ubuntu-focal-desktop-arm64'
