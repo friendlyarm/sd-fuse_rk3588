7fa7ebb6a63d4bc732f6661879a9a5ccaae8a093
Lawrence-Tang <tangzongsheng@gmail.com>
2025-09-16 16:46:29 +0800

    update wireless firmware

---
+++ prebuilt/firmware/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eu
+ROOTFS_DIR=$1
+CURRPATH=$PWD
+
+if [ -d "$ROOTFS_DIR/lib" ]; then
+	mkdir -p $ROOTFS_DIR/lib/firmware
+	LIBFIRMWARE="$(readlink -f $ROOTFS_DIR/lib/firmware)"
+else
+	mkdir -p $ROOTFS_DIR/usr/lib/firmware
+	LIBFIRMWARE="$(readlink -f $ROOTFS_DIR/usr/lib/firmware)"
+fi
+
+(cd $ROOTFS_DIR && {
+	# apply etc/firmware
+	[ -L etc/firmware ] && rm -f etc/firmware
+	cp -af $CURRPATH/files/etc/* etc/
+
+	# apply system/etc system/vendor
+	[ -d system ] || mkdir system
+	cp -af $CURRPATH/files/system/* system/
+
+	# apply /usr/lib/firmware or /lib/firmware
+	cp -af $CURRPATH/files/usr/lib/firmware/* "${LIBFIRMWARE}/"
+
+	# apply regulatory.db and regulatory.db.p7s
+	if [ ! -f "${LIBFIRMWARE}/regulatory.db" -a ! -L "${LIBFIRMWARE}/regulatory.db" ]; then
+		cp -f $CURRPATH/files2/usr/lib/firmware/regulatory.db "${LIBFIRMWARE}/"
+		cp -f $CURRPATH/files2/usr/lib/firmware/regulatory.db.p7s "${LIBFIRMWARE}/"
+	fi
+	if [ ! -f "${LIBFIRMWARE}/regulatory.db.p7s" -a ! -L "${LIBFIRMWARE}/regulatory.db.p7s" ]; then
+		cp -f $CURRPATH/files2/usr/lib/firmware/regulatory.db "${LIBFIRMWARE}/"
+		cp -f $CURRPATH/files2/usr/lib/firmware/regulatory.db.p7s "${LIBFIRMWARE}/"
+	fi
+})
